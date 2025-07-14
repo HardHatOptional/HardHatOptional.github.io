4955087a72de6dcf0d8757c702ed43d574473597
HardHatOptional <endsley.co@gmail.com>
2025-07-13 21:13:57 -0700

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import ResumeTruthMatcher from './ResumeTruthMatcher';
+
+function App() {
+  return (
+    <div className="App">
+      <ResumeTruthMatcher />
+    </div>
+  );
+}
+
+export default App;
